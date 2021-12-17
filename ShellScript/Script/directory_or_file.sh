59afa5715b2cdbb72a313f972cb9aeeff3cbab2a
Deekshith SN <deekshithsn@gmail.com>
2021-12-17 13:52:49 +0530

    Update directory_or_file.sh

--- ShellScript/Script/directory_or_file.sh
+++ ShellScript/Script/directory_or_file.sh
@@ -1,3 +1,15 @@
+# This script help us in identifying supplied file or directory exsists or not
+# Maintainer DeekshithSN
+# How to run the script 
+#      directory_or_file.sh -f filename
+#      directory_or_file.sh -d directoryname
+# Examples -
+#       directory_or_file.sh -f 
+#           o/p - the options might be wrong please check script details
+#       directory_or_file.sh -d
+#           o/p - the options might be wrong please check script details
+#       directory_or_file.sh -d <directory_name>
+#       directory_or_file.sh -d <filename_name>
 set -x                         # very useful when we want debug the script ( also we will call this as debug mode )
 if [ "$1" = "-f" -a -n "$2" ] 
 then
